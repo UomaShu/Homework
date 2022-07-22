7179ed80d218375c30aa9da703ae54e96f1f0aab
UomaShu <rozariokun@gmail.com>
2022-07-22 19:07:54 +0300

    кекнул
    Кек

---
+++ homework1.js
@@ -0,0 +1,7 @@
+let age, name, lastName, hobbie, mother;
+age = prompt('Сколько тебе лет?');
+name = prompt('Как тебя зовут');
+lastName = prompt ('Какая у тебя фамилия?');
+hobbie = prompt('Какие у тебя хобби ?');
+mother = prompt ('Любишь ли ты маму?');
+alert (Тебе ${age} лет, Твое полное имя это ${name} ${lastName}, ты увлекаешься ${hobbie} Любишь ли ты маму ? - ${mother} )
---
+++ homework2.js
@@ -0,0 +1,7 @@
+let firstNumber, secondNumber, thirdNumber, fourthNumber, result, secondResult;
+firstNumber = prompt('Введите первое число');
+secondNumber = prompt('Введите второе число');
+thirdNumber = prompt('Введите третье число');
+fourthNumber = prompt('Введите четвертое число');
+result = alert (firstNumber * secondNumber * thirdNumber *fourthNumber);
+secondResult = alert(firstNumber ** fourthNumber);
